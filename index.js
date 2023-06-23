1a8046e2c73acf76ff61660414e9d9e03a4f5962
Gustavo Rigoni <gustrigoni@gmail.com>
2023-06-23 16:11:55 -0300

    added a automation do create new users

---
+++ index.js
@@ -0,0 +1,79 @@
+import axios from 'axios';
+import FormData from 'form-data';
+import nodeReadline from 'readline';
+
+const URL_BFF = process.env.URL_BFF;
+
+const readline = nodeReadline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const promptInput = async (pergunta) => {
+  const respostaPrompt  = await new Promise((resolve, reject) => {
+    readline.question(`${pergunta}\n`, resposta => {
+      readline.close();
+      return resolve(resposta);
+    });
+  })
+
+  return respostaPrompt;
+}
+
+const executar = async () => {
+  let senhaEspecifica = null;
+
+  const senhaDigitada = await promptInput('Digite uma senha caso queira uma especifica...');
+
+  if (senhaDigitada) {
+    senhaEspecifica = senhaDigitada;
+  }
+
+  try {
+    const form = new FormData();
+
+    form.append('acao', 'gerar_pessoa');
+    form.append('sexo', 'I');
+    form.append('pontuacao', 'S');
+    form.append('idade', '0');
+    form.append('txt_qtde', '1');
+
+    const { data: [ dadoPessoal ]} = await axios.post('https://www.4devs.com.br/ferramentas_online.php', form);
+
+    const senhaUsuario = senhaEspecifica ?? dadoPessoal.senha;
+    const celular = dadoPessoal.celular.replace(' ', '');
+    const dataNascimento = dadoPessoal.data_nasc.split('/').reverse().toString().replaceAll(',', '');
+
+    const { data: { data }} = await axios.post(`${URL_BFF}/login/create`, {
+      mail: dadoPessoal.email,
+      celular: celular,
+      senha: senhaUsuario,
+      termo_aceite_sn: "S",
+      nome: dadoPessoal.nome,
+      nasc: dataNascimento,
+      sexo: dadoPessoal.sexo.toLowerCase() === 'masculino' ? 'M' : 'F',
+      cpf: dadoPessoal.cpf,
+      google_id: "",
+      facebook_id: "",
+      apple_id: "",
+      voucher: "",
+      grp: "no-safety-mails"
+    });
+
+    const retorno = {
+      nome: data.nome,
+      email: data.mail,
+      celular: celular,
+      senha: senhaUsuario,
+      ssid: data.ssid,
+      idPaciente: data.id_paciente,
+      idUsuario: data.id_usuario
+    }
+    
+    console.log('[SUCESSO] Usuário cadastrado', retorno);
+  } catch (error) {
+    console.error('[ERRO] Não foi possível prosseguir', error)
+  }
+}
+
+executar();
